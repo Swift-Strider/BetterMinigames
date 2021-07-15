82eee2ad0297328d0e021b598818a736da595c21
Stew <dajonreal@gmail.com>
2021-07-15 00:01:50 -0500

    Added Game and GameCache

---
+++ src/DiamondStrider1/BetterMinigames/GameCache.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ *   BetterMinigames is a plugin that adds the abillity to make gamemodes for PocketMine Servers
+ *   Copyright (C) 2021  DiamondStrider
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+declare(strict_types=1);
+
+namespace DiamondStrider1\BetterMinigames;
+
+use DiamondStrider1\BetterMinigames\data\IDataProvider;
+use DiamondStrider1\BetterMinigames\types\DeserializationResult;
+use DiamondStrider1\BetterMinigames\types\Game;
+use pocketmine\utils\TextFormat as TF;
+
+class GameCache
+{
+    /** @var Game[] $games */
+    private $games = [];
+
+    /** @var IDataProvider $config */
+    private $config;
+
+    public function __construct(IDataProvider $config)
+    {
+        $this->config = $config;
+    }
+
+    public function createGame(string $id): Game
+    {
+        return $this->games[$id] = new Game($id);
+    }
+
+    public function getGame(string $id): ?Game
+    {
+        return isset($this->games[$id]) ? $this->games[$id] : null;
+    }
+
+    /** @return string[] */
+    public function getAllGames(): array
+    {
+        return $this->games;
+    }
+
+    public function load(bool $reloadConfig = false): DeserializationResult
+    {
+        if ($reloadConfig) {
+            $this->config->reload();
+        }
+        $data = $this->config->getAll();
+        return $this->loadFromArray($data);
+    }
+
+    public function save(): void
+    {
+        $entries = $this->saveToArray();
+        $this->config->setAll($entries);
+        $this->config->save("Data for all games is stored here");
+    }
+
+    private function loadFromArray(array $entries): DeserializationResult
+    {
+        $ret = new DeserializationResult;
+        $this->games = [];
+        foreach ($entries as $id => $gameData) {
+            $game = new Game($id);
+            $result = $game->loadFromArray($gameData);
+            $ret->addResult(
+                $result,
+                sprintf("%s (%d errors): %s", $id, count($result->getErrors()), TF::YELLOW),
+                sprintf("%s (%d warnings): %s", $id, count($result->getWarnings()), TF::YELLOW),
+                TF::RESET . ", " . TF::YELLOW
+            );
+            if ($result->hasErrors()) {
+                // TODO: store invalid entries in array form, so admins can fix errors after shutting down the server
+                continue;
+            }
+            $this->games[$id] = $game;
+        }
+        return $ret;
+    }
+
+    private function saveToArray(): array
+    {
+        $data = [];
+        foreach ($this->games as $id => $entry) {
+            $data[$id] = $entry->saveToArray();
+        }
+        return $data;
+    }
+}
---
+++ src/DiamondStrider1/BetterMinigames/types/Game.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ *   BetterMinigames is a plugin that adds the abillity to make gamemodes for PocketMine Servers
+ *   Copyright (C) 2021  DiamondStrider
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+declare(strict_types=1);
+
+namespace DiamondStrider1\BetterMinigames\types;
+
+use DiamondStrider1\BetterMinigames\utils\Utils;
+use ErrorException;
+use OutOfBoundsException;
+
+class Game
+{
+    /** @var string $id */
+    private $id;
+    /** @var string $name */
+    private $name;
+    /** @var string[] $arenas
+     * $mapName => $arenaID
+     */
+    private $maps;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function addMap(string $mapName, string $arenaID): void
+    {
+        $this->maps[$mapName] = $arenaID;
+    }
+
+    public function removeMap(string $mapName): void
+    {
+        if (isset($this->maps[$mapName])) {
+            unset($this->maps[$mapName]);
+        }
+    }
+
+    public function loadFromArray(array $data): DeserializationResult
+    {
+        $ret = new DeserializationResult;
+
+        try {
+            $this->maps = $data["maps"];
+        } catch (ErrorException $e) {
+            $ret->addError($e->getMessage());
+        }
+
+        return $ret;
+    }
+
+    public function saveToArray()
+    {
+        $data = [];
+
+        $data["name"] = $this->name;
+        $data["maps"] = $this->maps;
+
+        return $data;
+    }
+}
